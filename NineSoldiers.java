d813264d99ce627271c0eb513c9325c1ef163ffa
yehboiilance <lancebotha0705@gmail.com>
2024-03-05 21:15:35 +0200

    Tuesday night changes

--- NineSoldiers.java
+++ NineSoldiers.java
@@ -1,5 +1,10 @@
+import java.util.*;
+import java.io.*;
+
 public class NineSoldiers{
-    public static void main(String[] args){
-        System.out.println("Hello, world! This is java.");
-    }
+    
+    
+
+    
+    
 }
